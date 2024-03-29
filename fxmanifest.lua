202aea7f330845b63b743e5d11be9def1d325815
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-03-29 23:08:42 +0000

    Hotdog

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+
+files {
+	'data/**/handling.meta',
+	'data/**/vehicles.meta',
+}
+
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
